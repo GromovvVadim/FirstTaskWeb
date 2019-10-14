3071351f648195073c929db9e61d6c0410602863
vadim <vadimgromov1403@gmail.com>
2019-10-14 22:45:40 +0300

    Reservation

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,5 @@
 (function($) {
-    "use strict";
-	
-	/* ..............................................
-	Loader 
-    ................................................. */
-	
-	$(window).on('load', function() { 
-		$('.preloader').fadeOut(); 
-		$('#preloader').delay(550).fadeOut('slow'); 
-		$('body').delay(450).css({'overflow':'visible'});
-	});
-	
-	/* ..............................................
-    Fixed Menu
-    ................................................. */
-    
-	$(window).on('scroll', function () {
-		if ($(window).scrollTop() > 50) {
-			$('.top-header').addClass('fixed-menu');
-		} else {
-			$('.top-header').removeClass('fixed-menu');
-		}
-	});
-	
+   
 	/* ..............................................
     Gallery
     ................................................. */
@@ -35,24 +12,6 @@
 	});
 	
 	$( ".cover-slides ul li" ).append( "<div class='overlay-background'></div>" );
-	
-	/* ..............................................
-    Map Full
-    ................................................. */
-	
-	$(document).ready(function(){ 
-		$(window).on('scroll', function () {
-			if ($(this).scrollTop() > 100) { 
-				$('#back-to-top').fadeIn(); 
-			} else { 
-				$('#back-to-top').fadeOut(); 
-			} 
-		}); 
-		$('#back-to-top').click(function(){ 
-			$("html, body").animate({ scrollTop: 0 }, 600); 
-			return false; 
-		}); 
-	});
 	
 	/* ..............................................
     Special Menu
@@ -81,26 +40,20 @@
 		animation: 'fadeIn',
 		noScrollbars: true
 	});
-	
-	
-	
-	/* ..............................................
-    Datepicker
-    ................................................. */
-	
-	$('.datepicker').pickadate();
-	
-	$('.time').pickatime();
-	
-	
-	
-	
-	
+		
 }(jQuery));
 
-function clearFields() {
+function clearFieldsContact() {
     document.getElementById("name").value=""
     document.getElementById("email").value=""
     document.getElementById("subject").value=""
     document.getElementById("message").value=""
 }
+
+function clearFieldsReservation() {
+    document.getElementById("datetime1").value=""
+    document.getElementById("person1").value="Number of people"
+    document.getElementById("name1").value=""
+    document.getElementById("email1").value=""
+}
+
